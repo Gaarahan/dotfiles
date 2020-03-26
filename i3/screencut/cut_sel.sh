1e490e7b4c89d7397b0c090c54dbde777a3992b0
gaarahan <815199630@qq.com>
2020-03-27 00:31:49 +0800

    change polybar-config to splited module file

--- i3/screencut/cut_sel.sh
+++ i3/screencut/cut_sel.sh
@@ -1,1 +1,1 @@
-scrot -s -b -m -e 'xclip -selection clipoard -t "image/png" $f && mv $f ~/Picture/ScreenShortcut/%Y-%m-%d_%H:%m_select.png'
+scrot -q 100 -s -b -m -e 'xclip -selection clipoard -t "image/png" $f && mv $f ~/Picture/ScreenShortcut/%Y-%m-%d_%H:%m_select.png'
