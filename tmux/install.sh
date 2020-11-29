46f4a1f3cb1c7876abb3d0c7848e78290b29ad0d
gaarahan <gaarahan@foxmail.com>
2020-11-29 17:53:30 +0800

    add simple install shell

---
+++ tmux/install.sh
@@ -0,0 +1,3 @@
+autoStartStr = 'if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
+    tmux attach -t default || tmux new -s default
+fi'
--- vim/script/start.sh
+++ vim/script/start.sh
@@ -1,3 +1,6 @@
 doInstall() {
-	echo "install ~"
+	echo "installing ~"
+  mkdir -p ~/.config/nvim
+  # https://vi.stackexchange.com/questions/12794/how-to-share-config-between-vim-and-neovim
+  printf "set runtimepath^=~/.vim\nruntimepath+=~/.vim/after\nlet &packpath=&runtimepath\nsource ~/.vimrc" >> ~/.config/nvim
 }
