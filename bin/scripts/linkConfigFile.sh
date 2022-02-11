61551672b0fd4ec2b5335ddc28a72c408ac48cfa
Gaarahan <gaarahan@foxmail.com>
2022-02-11 22:27:22 +0800

    replace path

--- bin/scripts/linkConfigFile.sh
+++ bin/scripts/linkConfigFile.sh
@@ -4,10 +4,10 @@ read -r
 if [ "$REPLY" == 'y' ] || [ "$REPLY" == 'Y' ]; then
   info "Start config zsh"
   if [ -e "$ZSHRC" ]; then
-    rm "$ZSHRC.bak" >/dev/null
+    rm "$ZSHRC.bak" > /dev/null 2>&1
     mv "$ZSHRC" "$ZSHRC.bak"
   fi
-  ln -s "$(getPath "../zsh/zshrc")" "$ZSHRC"
+  ln -s "$(getPath "../../zsh/zshrc")" "$ZSHRC"
   ok "Config success"
 
 else
@@ -22,26 +22,26 @@ NVIMRC="$NVIM_HOME/init.vim"
 info "Start config nvim"
 
 if [ -e "$NVIMRC" ]; then
-  rm "$NVIMRC.bak" >/dev/null
+  rm "$NVIMRC.bak" >/dev/null 2>&1
   mv "$NVIMRC" "$NVIMRC.bak"
 fi
 
 if [ -e "$VIMRC" ]; then
-  rm "$VIMRC.bak" >/dev/null
+  rm "$VIMRC.bak" >/dev/null 2>&1
   mv "$VIMRC" "$VIMRC.bak"
 fi
 
-ln -s "$(getPath "../vim/vimrc")" "$VIMRC"
+ln -s "$(getPath "../../vim/vimrc")" "$VIMRC"
 if [[ ! -d "$NVIM_HOME" ]]; then
   mkdir "$NVIM_HOME"
 fi
-ln -s "$(getPath "../vim/init.vim")" "$NVIMRC"
+ln -s "$(getPath "../../vim/init.vim")" "$NVIMRC"
 
 if [[ -d "$VIM_HOME" ]]; then
   mv "$VIM_HOME" "${VIM_HOME}_back"
   ok "BackUp $VIM_HOME to ${VIM_HOME}_back"
 fi
-ln -s "$(getPath "../vim/vim")" "$HOME/.vim"
+ln -s "$(getPath "../../vim/vim")" "$HOME/.vim"
 ok "Config vim success, you can now open it and run 'PlugInstall'.
 After install plugin, you could install your perfer language support, like:
 CocInstall coc-html coc-css coc-tsserver coc-vetur coc-json coc-sh coc-clangd coc-markdownlint"
@@ -55,9 +55,9 @@ if [ "$REPLY" == 'y' ] || [ "$REPLY" == 'Y' ]; then
   info "Start config tmux"
 
   if [ -e "$TMUX_CONF" ]; then
-    rm "$TMUX_CONF.bak" >/dev/null
+    rm "$TMUX_CONF.bak" >/dev/null 2>&1
     mv "$TMUX_CONF" "$TMUX_CONF.bak"
   fi
-  ln -s "$(getPath "../tmux/tmux.conf")" "$TMUX_CONF"
+  ln -s "$(getPath "../../tmux/tmux.conf")" "$TMUX_CONF"
   ok "Config success"
 fi
