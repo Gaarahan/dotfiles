bc5fb88392b89581fd841874cc67b441b1abe66f
Gaarahan <gaarahan@foxmail.com>
2022-08-24 11:37:16 +0800

    Update prepareBeforeStart.sh

--- bin/scripts/prepareBeforeStart.sh
+++ bin/scripts/prepareBeforeStart.sh
@@ -33,8 +33,7 @@ fi
 # ###########################################################
 
 running "checking homebrew..."
-brew_bin= which brew 2>&1 > /dev/null
-if [[ $? != 0 ]]; then
+if $(type brew &> /dev/null) ; then
   action "installing homebrew"
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   if [[ $? != 0 ]]; then
