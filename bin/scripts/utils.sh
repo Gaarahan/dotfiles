9b97ee98a7446ad73ce54ab1b296896312c2df0d
Gaarahan <gaarahan@foxmail.com>
2022-08-24 22:00:53 +0800

    Update utils.sh

--- bin/scripts/utils.sh
+++ bin/scripts/utils.sh
@@ -52,7 +52,7 @@ brewCheckOrInstall() {
     brew list $1 > /dev/null 2>&1 | true
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
         action "brew install $1 $2"
-        brew install $1 $2
+        HOMEBREW_NO_AUTO_UPDATE=1 brew install $1 $2
         if [[ $? != 0 ]]; then
             error "failed to install $1! aborting..."
             # exit -1
