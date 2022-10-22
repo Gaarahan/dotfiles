f1c509c4ac2962baa866b1e356312d3f0f3303ed
Gaarahan <gaarahan@foxmail.com>
2022-10-22 23:24:09 +0800

    change script

--- bin/scripts/startFromScratch.sh
+++ bin/scripts/startFromScratch.sh
@@ -43,6 +43,10 @@ running "gem install neovim"
 gem install neovim
 ok
 
+running "install neovim packer"
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+ok
+
 brew update && brew upgrade && brew cleanup
 
 bot "All done"
