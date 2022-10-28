978695d20f6bf7a7f0a1f3984de222a85770bd09
Gaarahan <gaarahan@foxmail.com>
2022-10-28 21:31:27 +0800

    feat: autoinstall plugins

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -1,3 +1,15 @@
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
 local packer = require('packer')
 local use = packer.use
 
@@ -64,5 +76,7 @@ packer.startup(function()
     use { 'junegunn/fzf', run = ":call fzf#install()" }
     use { 'junegunn/fzf.vim' }
 
-
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
