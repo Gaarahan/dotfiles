e1706a92b93bdaf7a3a568170e8007ab13d6ee4c
Gaarahan <gaarahan@foxmail.com>
2022-11-29 14:58:40 +0800

    Create formatter.lua

---
+++ nvim/lua/usermod/formatter.lua
@@ -0,0 +1,34 @@
+-- Utilities for creating configurations
+local util = require "formatter.util"
+
+-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+require("formatter").setup {
+  -- Enable or disable logging
+  logging = true,
+  -- Set the log level
+  log_level = vim.log.levels.WARN,
+  -- All formatter configurations are opt-in
+  filetype = {
+    -- Formatter configurations for filetype "lua" go here
+    -- and will be executed in order
+    lua = {
+      require("formatter.filetypes.lua").stylua,
+    },
+
+    typescriptreact = {
+      require("formatter.filetypes.typescriptreact").prettierd,
+    },
+
+    typescript = {
+      require("formatter.filetypes.typescript").prettierd,
+    },
+
+    -- Use the special "*" filetype for defining formatter configurations on
+    -- any filetype
+    ["*"] = {
+      -- "formatter.filetypes.any" defines default configurations for any
+      -- filetype
+      require("formatter.filetypes.any").remove_trailing_whitespace
+    }
+  }
+}
