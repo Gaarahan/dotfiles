2cc86f086cf11bed9dfa0a525b7a58191ae21393
Gaarahan <gaarahan@foxmail.com>
2023-01-29 10:54:53 +0800

    Update telescope.lua

--- nvim/lua/usermod/telescope.lua
+++ nvim/lua/usermod/telescope.lua
@@ -1,21 +1,61 @@
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
+local utils = require("usermod.utils")
+local lga_actions = require("telescope-live-grep-args.actions")
+local map = utils.map
+local lua_fn = utils.lua_fn
 
-require('telescope').setup{
-  defaults = {
-    vimgrep_arguments = {
-      "rg",
-      "--color=never",
-      "--no-heading",
-      "--with-filename",
-      "--line-number",
-      "--column",
-      "--smart-case"
-    }
-  }
-}
+require("telescope").setup({
+	defaults = {
+		vimgrep_arguments = {
+			"rg",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+		},
+	},
+	extensions = {
+		live_grep_args = {
+			auto_quoting = true, -- enable/disable auto-quoting
+			mappings = { -- extend mappings
+				i = {
+					["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+					["<C-h>"] = lga_actions.quote_prompt({ postfix = " --no-ignore " }),
+				},
+			},
+		},
+	},
+})
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fr', builtin.resume, {})
-vim.keymap.set('n', '<leader>fb', builtin.current_buffer_fuzzy_find, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+map(
+	"n|<leader>ff",
+	lua_fn(function()
+		builtin.find_files()
+	end)
+)
+map(
+	"n|<leader>fg",
+	lua_fn(function()
+		require("telescope").extensions.live_grep_args.live_grep_args()
+	end)
+)
+map(
+	"n|<leader>fr",
+	lua_fn(function()
+		builtin.resume()
+	end)
+)
+map(
+	"n|<leader>fb",
+	lua_fn(function()
+		builtin.current_buffer_fuzzy_find()
+	end)
+)
+map(
+	"n|<leader>fh",
+	lua_fn(function()
+		builtin.help_tags()
+	end)
+)
