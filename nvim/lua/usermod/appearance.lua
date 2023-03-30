0c4950aa74f2887ab9837c7a47a3e799c73db8ee
Gaarahan <gaarahan@foxmail.com>
2023-03-30 15:59:40 +0800

    Update appearance.lua

--- nvim/lua/usermod/appearance.lua
+++ nvim/lua/usermod/appearance.lua
@@ -11,52 +11,6 @@ set_option({
 
 vim.cmd("color gruvbox")
 
--- { Barbar : }"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
--- Set barbar's options
-vim.g.bufferline = {
-	animation = true,
-	auto_hide = false,
-	tabpages = true,
-	closable = true,
-	clickable = true,
-	icons = true,
-
-	-- If set, the icon color will follow its corresponding buffer
-	-- highlight group. By default, the Buffer*Icon group is linked to the
-	-- Buffer* group (see Highlighting below). Otherwise, it will take its
-	-- default value as defined by devicons.
-	icon_custom_colors = true,
-
-	-- Configure icons on the bufferline.
-	icon_separator_active = "▎",
-	icon_separator_inactive = "▎",
-	icon_close_tab = "",
-	icon_close_tab_modified = "●",
-	icon_pinned = "車",
-
-	-- Sets the maximum padding width with which to surround each tab
-	maximum_padding = 1,
-
-	-- Sets the maximum buffer name length.
-	maximum_length = 30,
-
-	-- If set, the letters for each buffer in buffer-pick mode will be
-	-- assigned based on their name. Otherwise or in case all letters are
-	-- already assigned, the behavior is to assign letters in order of
-	-- usability (see order below)
-	semantic_letters = true,
-
-	-- New buffer letters are assigned in this order. This order is
-	-- optimal for the qwerty keyboard layout but might need adjustement
-	-- for other layouts.
-	letters = "asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP",
-
-	-- Sets the name of unnamed buffers. By default format is "[Buffer X]"
-	-- where X is the buffer number. But only a static string is accepted here.
-	no_name_title = nil,
-}
-
 local function hi(name, fg, bg, attr)
 	vim.cmd("hi! clear " .. name)
 
