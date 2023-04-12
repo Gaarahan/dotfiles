f7e78b5b30fecd576b3c303d973499b3f2acfadf
Gaarahan <gaarahan@foxmail.com>
2023-04-12 11:12:35 +0800

    Update appearance.lua

--- nvim/lua/usermod/appearance.lua
+++ nvim/lua/usermod/appearance.lua
@@ -67,6 +67,8 @@ require("dressing").setup({
 })
 
 -- { Home page: }"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+vim.g.startify_change_to_dir = 0
 vim.g.startify_lists = {
 	{ type = "dir", header = { "   MRU" .. vim.fn.getcwd() } },
 	{ type = "sessions", header = { "   Sessions" } },
