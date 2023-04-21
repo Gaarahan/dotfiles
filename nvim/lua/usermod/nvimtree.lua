424357645f9cebe222248d2619c53448de3f5784
Gaarahan <gaarahan@foxmail.com>
2023-04-21 12:26:26 +0800

    Update nvimtree.lua

--- nvim/lua/usermod/nvimtree.lua
+++ nvim/lua/usermod/nvimtree.lua
@@ -3,23 +3,11 @@ local map_cmd = utils.map_cmd
 
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
+
 vim.g.loaded_netrwPlugin = 1
-
-local function do_nothing(node) end
-
 -- use g? to show help page
 require("nvim-tree").setup({
 	sort_by = "case_sensitive",
-	view = {
-		adaptive_size = true,
-		mappings = {
-			list = {
-				{ key = "<C-[>", action = "dir_up" },
-				{ key = "-", action = "" },
-				{ key = "<Esc>", action = "do_nothing", action_cb = "do_nothing" },
-			},
-		},
-	},
 	renderer = {
 		group_empty = true,
 	},
