2a7e9a42496a917ae88896d532baf5cc23f3213c
Gaarahan <gaarahan@foxmail.com>
2023-04-27 10:29:31 +0800

    Change lualine setting

--- nvim/lua/usermod/appearance.lua
+++ nvim/lua/usermod/appearance.lua
@@ -11,38 +11,24 @@ set_option({
 vim.cmd("color gruvbox")
 
 local customize_onedark = require("lualine.themes.onedark")
-
 customize_onedark.normal.a.bg = "#5DD02E"
 
 -- { Status line: }"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 require("lualine").setup({
 	options = {
-		icons_enabled = true,
 		theme = customize_onedark,
 		component_separators = { left = "|", right = "|" },
 		section_separators = { left = "", right = "" },
-		disabled_filetypes = {},
-		always_divide_middle = true,
 	},
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
+		lualine_c = { { "filename", path = 1 } },
 		lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
 	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {},
-	extensions = {},
 })
 
 -- { Prompt: }"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
