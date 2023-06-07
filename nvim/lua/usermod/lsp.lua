245793f7034f763689865594614f11f54f902579
Gaarahan <gaarahan@foxmail.com>
2023-06-07 10:56:00 +0800

    Update lsp.lua

--- nvim/lua/usermod/lsp.lua
+++ nvim/lua/usermod/lsp.lua
@@ -1,3 +1,18 @@
+local lspconfig = require("lspconfig")
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.foldingRange = {
+	dynamicRegistration = false,
+	lineFoldingOnly = true,
+}
+
+local language_servers = require("lspconfig").util.available_servers()
+for _, ls in ipairs(language_servers) do
+	lspconfig[ls].setup({
+		capabilities = capabilities,
+	})
+end
+
 -- disable builtin diagnostic virtual_text
 vim.diagnostic.config({
 	virtual_text = false,
