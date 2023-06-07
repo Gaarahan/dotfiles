c67fd9438983c79fb28ad06317fdbec86dbc9517
Gaarahan <gaarahan@foxmail.com>
2023-06-07 10:56:30 +0800

    Update nvim-ufo.lua

--- nvim/lua/usermod/nvim-ufo.lua
+++ nvim/lua/usermod/nvim-ufo.lua
@@ -3,19 +3,6 @@ vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decr
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.foldingRange = {
-	dynamicRegistration = false,
-	lineFoldingOnly = true,
-}
-local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-for _, ls in ipairs(language_servers) do
-	require("lspconfig")[ls].setup({
-		capabilities = capabilities,
-		-- you can add other fields for setting up lsp server in this table
-	})
-end
-
 local handler = function(virtText, lnum, endLnum, width, truncate)
     local newVirtText = {}
     local suffix = (' 󰁂%d '):format(endLnum - lnum)
