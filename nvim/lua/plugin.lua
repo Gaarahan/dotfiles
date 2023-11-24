72a5f4c7b0ec325608a080de475ded3814d66f41
Gaarahan <gaarahan@foxmail.com>
2023-11-24 11:14:55 +0800

    Update plugin.lua

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -33,6 +33,24 @@ packer.startup(function()
 	use("yuttie/comfortable-motion.vim") -- move smooth when use like <C-f>
 	use("stevearc/dressing.nvim") -- beautify vim.ui
 	use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" }) -- improve vim fold
+	use({
+		"echasnovski/mini.hipatterns",
+		config = function()
+			local hipatterns = require("mini.hipatterns")
+			hipatterns.setup({
+				highlighters = {
+					-- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+					fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+					hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+					todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+					note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+					-- Highlight hex color strings (`#rrggbb`) using that color
+					hex_color = hipatterns.gen_highlighter.hex_color(),
+				},
+			})
+		end,
+	}) -- highlight todo and colors
 
 	--  plug for dir_tree
 	use({
@@ -79,6 +97,7 @@ packer.startup(function()
 
 	-- Autocompletion plugin
 	use("hrsh7th/nvim-cmp")
+	use("mattn/emmet-vim")
 	use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
 	use({ "L3MON4D3/LuaSnip", version = "<CurrentMajor>.*", run = "make install_jsregexp" }) -- Snippets plugin
 	use("windwp/nvim-ts-autotag")
