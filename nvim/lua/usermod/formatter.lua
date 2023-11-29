39ad45ddcc4ee65de81f9db77680adb0a8b47019
Gaarahan <815199630@qq.com>
2023-11-29 04:01:58 -0600

    feat: move to lazy

--- nvim/lua/usermod/formatter.lua
+++ nvim/lua/usermod/formatter.lua
@@ -1,50 +1,52 @@
--- Utilities for creating configurations
-local util = require("formatter.util")
+return {
+	"mhartington/formatter.nvim",
+	init = function()
+		-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+		require("formatter").setup({
+			-- Enable or disable logging
+			logging = true,
+			-- Set the log level
+			log_level = vim.log.levels.WARN,
+			-- All formatter configurations are opt-in
+			filetype = {
+				-- Formatter configurations for filetype "lua" go here
+				-- and will be executed in order
+				lua = {
+					require("formatter.filetypes.lua").stylua,
+				},
 
--- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-require("formatter").setup({
-	-- Enable or disable logging
-	logging = true,
-	-- Set the log level
-	log_level = vim.log.levels.WARN,
-	-- All formatter configurations are opt-in
-	filetype = {
-		-- Formatter configurations for filetype "lua" go here
-		-- and will be executed in order
-		lua = {
-			require("formatter.filetypes.lua").stylua,
-		},
+				javascript = {
+					require("formatter.filetypes.javascript").prettierd,
+				},
 
-		javascript = {
-			require("formatter.filetypes.javascript").prettierd,
-		},
+				javascriptreact = {
+					require("formatter.filetypes.javascriptreact").prettierd,
+				},
 
-		javascriptreact = {
-			require("formatter.filetypes.javascriptreact").prettierd,
-		},
+				html = {
+					require("formatter.filetypes.html").prettierd,
+				},
 
-		html = {
-			require("formatter.filetypes.html").prettierd,
-		},
+				css = {
+					require("formatter.filetypes.css").prettierd,
+				},
 
-		css = {
-			require("formatter.filetypes.css").prettierd,
-		},
+				typescriptreact = {
+					require("formatter.filetypes.typescriptreact").prettierd,
+				},
 
-		typescriptreact = {
-			require("formatter.filetypes.typescriptreact").prettierd,
-		},
+				typescript = {
+					require("formatter.filetypes.typescript").prettierd,
+				},
 
-		typescript = {
-			require("formatter.filetypes.typescript").prettierd,
-		},
-
-		-- Use the special "*" filetype for defining formatter configurations on
-		-- any filetype
-		["*"] = {
-			-- "formatter.filetypes.any" defines default configurations for any
-			-- filetype
-			require("formatter.filetypes.any").remove_trailing_whitespace,
-		},
-	},
-})
+				-- Use the special "*" filetype for defining formatter configurations on
+				-- any filetype
+				["*"] = {
+					-- "formatter.filetypes.any" defines default configurations for any
+					-- filetype
+					require("formatter.filetypes.any").remove_trailing_whitespace,
+				},
+			},
+		})
+	end,
+}
