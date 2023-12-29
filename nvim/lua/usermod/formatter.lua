d8e54d70a27e7285682ec301839ae2a336dd3da6
Gaarahan <gaarahan@foxmail.com>
2023-12-29 16:29:11 +0800

    Update formatter.lua

--- nvim/lua/usermod/formatter.lua
+++ nvim/lua/usermod/formatter.lua
@@ -1,52 +1,14 @@
 return {
-	"mhartington/formatter.nvim",
+	"nvimdev/guard.nvim",
+	dependencies = {
+		"nvimdev/guard-collection",
+	},
 	init = function()
-		-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-		require("formatter").setup({
-			-- Enable or disable logging
-			logging = true,
-			-- Set the log level
-			log_level = vim.log.levels.WARN,
-			-- All formatter configurations are opt-in
-			filetype = {
-				-- Formatter configurations for filetype "lua" go here
-				-- and will be executed in order
-				lua = {
-					require("formatter.filetypes.lua").stylua,
-				},
-
-				javascript = {
-					require("formatter.filetypes.javascript").prettierd,
-				},
-
-				javascriptreact = {
-					require("formatter.filetypes.javascriptreact").prettierd,
-				},
-
-				html = {
-					require("formatter.filetypes.html").prettierd,
-				},
-
-				css = {
-					require("formatter.filetypes.css").prettierd,
-				},
-
-				typescriptreact = {
-					require("formatter.filetypes.typescriptreact").prettierd,
-				},
-
-				typescript = {
-					require("formatter.filetypes.typescript").prettierd,
-				},
-
-				-- Use the special "*" filetype for defining formatter configurations on
-				-- any filetype
-				["*"] = {
-					-- "formatter.filetypes.any" defines default configurations for any
-					-- filetype
-					require("formatter.filetypes.any").remove_trailing_whitespace,
-				},
-			},
+		require("guard").setup({
+			-- the only options for the setup function
+			fmt_on_save = true,
+			-- Use lsp if no formatter was defined for this filetype
+			lsp_as_default_formatter = true,
 		})
 	end,
 }
