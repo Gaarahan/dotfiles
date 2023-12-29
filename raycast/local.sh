9235c735782b3b82f77cac25a702176b8b1669cf
Gaarahan <gaarahan@foxmail.com>
2023-12-29 17:58:16 +0800

    Update local.sh

--- raycast/local.sh
+++ raycast/local.sh
@@ -2,15 +2,21 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title LOCAL
+# @raycast.title JARVIS
 # @raycast.mode compact
 
 # Optional parameters:
 # @raycast.icon 🤖
-# @raycast.argument1 { "type": "text", "placeholder": "PORT" }
+# @raycast.argument1 { "type": "text", "placeholder": "PORT or PACKAGE" }
 # @raycast.packageName local
 
 # Documentation:
 # @raycast.description Open local by port
 
-open "https://localhost:$1"
+if [[ $string =~ ^[0-9]+$ ]]; then
+  # if all number: open localhost
+  open "https://localhost:$1"
+else 
+  # else search in yuyan
+  open "https://yuyan.antfin-inc.com/search?q=$1"
+fi
