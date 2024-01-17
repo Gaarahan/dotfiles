26dd72e06779e0ea45383e58226a1a9978f521d1
Gaarahan <gaarahan@foxmail.com>
2024-01-17 11:08:25 +0800

    Update formatter.lua

--- nvim/lua/usermod/formatter.lua
+++ nvim/lua/usermod/formatter.lua
@@ -6,7 +6,7 @@ return {
 	init = function()
 		require("guard").setup({
 			-- the only options for the setup function
-			fmt_on_save = true,
+			fmt_on_save = false,
 			-- Use lsp if no formatter was defined for this filetype
 			lsp_as_default_formatter = true,
 		})
