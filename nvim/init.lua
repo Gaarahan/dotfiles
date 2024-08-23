7e65d252dec63fe37dc6da72dd214d8ba4cfd422
Gaarahan <815199630@qq.com>
2024-08-23 14:22:20 +0800

    feat: restruct priject & add dirctory as cmp source

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,6 +11,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("plugin")
-
 require("config")
--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -1,19 +1,20 @@
 -- { Load module: }"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 local conf_modules = {
+	"plugins.plugin", -- init plugin
+  
+	-- config ---
 	"basic",
 	"keymap",
 	"appearance",
 	"lsp",
 	"nvimtree",
 	"gitsigns",
-	"telescope",
-	"completion",
-	"formatter",
 	"nvim-ufo",
-}
-
-for i = 1, #conf_modules do
+  }
+  
+  for i = 1, #conf_modules do
 	local modulePath = "./usermod." .. conf_modules[i]
 	require(modulePath)
-end
+  end
+  
