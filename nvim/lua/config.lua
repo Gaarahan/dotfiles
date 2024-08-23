861f2435402271031a84ae9050cee0846ecdbd20
Gaarahan <815199630@qq.com>
2024-08-23 14:25:18 +0800

    fix: fix format

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -2,7 +2,7 @@
 
 local conf_modules = {
 	"plugins.plugin", -- init plugin
-  
+
 	-- config ---
 	"basic",
 	"keymap",
@@ -12,9 +12,8 @@ local conf_modules = {
 	"gitsigns",
 	"nvim-ufo",
   }
-  
+
   for i = 1, #conf_modules do
 	local modulePath = "./usermod." .. conf_modules[i]
 	require(modulePath)
   end
-  
