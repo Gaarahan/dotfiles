3794d53a6a91997b11c66d8a000ddbb7efc25cd9
Gaarahan <gaarahan@foxmail.com>
2025-07-11 17:11:59 +0800

    Update appearance.lua

--- nvim/lua/usermod/appearance.lua
+++ nvim/lua/usermod/appearance.lua
@@ -34,19 +34,6 @@ require("lualine").setup({
 -- { Prompt: }"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 require("dressing").setup({
-	select = {
-		get_config = function(opts)
-			if opts.kind == "legendary.nvim" then
-				return {
-					telescope = {
-						sorter = require("telescope.sorters").fuzzy_with_index_bias({}),
-					},
-				}
-			else
-				return {}
-			end
-		end,
-	},
 	input = {
 		enabled = false,
 	},
